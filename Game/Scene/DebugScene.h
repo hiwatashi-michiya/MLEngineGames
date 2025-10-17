60e61f3c56a999e24fec94cb5677fb4fa914eb4c
hiwatashi-michiya <115054875+hiwatashi-michiya@users.noreply.github.com>
2025-10-17 10:28:52 +0900

    exe出力場所をGeneratedに変更

--- Game/Scene/DebugScene.h
+++ Game/Scene/DebugScene.h
@@ -2,7 +2,13 @@
 #include "Scene/BaseScene.h"
 #include "Model/Model.h"
 #include "Object/Camera.h"
+#include "Sprite/Sprite.h"
+#include "Audio/Audio.h"
 
+/// <summary>
+/// デバッグシーン、エンジン機能理解用
+/// シーンはmain.cppで初期シーンのポインタを渡す
+/// </summary>
 class DebugScene : public BaseScene
 {
 public:
@@ -21,7 +27,14 @@ private:
 
 	MLEngine::Object::Camera camera_;
 
+	MLEngine::Resource::Audio se1_;
+
 	MLEngine::Resource::RigidModel model_;
+	//Vector3宣言
+	MLEngine::Math::Vector3 position_;
+	//Matrix4x4宣言
+	MLEngine::Math::Matrix4x4 matrix_;
+	
 
 };
 
