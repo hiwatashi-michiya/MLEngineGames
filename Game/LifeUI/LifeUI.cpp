e10b6c83bdb2c2cb42277a1daa7b64c1d3727f2e
aoaomidri <aoaomidori2723@gmail.com>
2025-12-08 16:38:38 +0900

    体力UIの調整

--- Game/LifeUI/LifeUI.cpp
+++ Game/LifeUI/LifeUI.cpp
@@ -9,20 +9,23 @@ LifeUI::LifeUI(Player* player){
 	texture_.Load("./Resources/white.png");
 
 	meter_.reset(MLEngine::Resource::Sprite2D::Create(texture_,plMeterPos_, plColor_));
-
 	//必須となる情報の読み込み
 	texture_.Load("./Resources/frame.png");
 
 	frame_.reset(MLEngine::Resource::Sprite2D::Create(texture_, plFramePos_, plColor_));
+
 }
 
 void LifeUI::Initialize() {
 	plMeterPos_ = { 60.0f,520.0f };
-	plMeterSize_ = { 65.0f,520.0f };
+	plMeterSize_ = { 45.0f,223.0f };
 
 	meter_->anchorPoint = { 0.5f,1.0f };
 
 	frame_->anchorPoint = { 0.5f,1.0f };
+
+
+	plFrameSize_ = { 100.0f,400.0f };
 }
 
 
@@ -35,8 +38,8 @@ void LifeUI::Update() {
 	meter_->position = plMeterPos_;	
 	meter_->size = plMeterSize_;
 
-	plFramePos_ = { plMeterPos_.x,plMeterPos_.y + 15.0f };
-	plFrameSize_ = { plMeterSize_.x * 1.5f,PLyMax_ + 30.0f };
+	plFramePos_ = { plMeterPos_.x,plMeterPos_.y + 20.0f };
+	//plFrameSize_ = { plMeterSize_.x * 1.5f,PLyMax_ + 30.0f };
 
 	frame_->position = plFramePos_;
 	frame_->size = plFrameSize_;
@@ -54,7 +57,8 @@ void LifeUI::DebugDraw() {
 
 	ImGui::DragFloat2("座標", &plMeterPos_.x, 0.5f, 0.0f, 1280.0f);
 
-	ImGui::DragFloat2("サイズ", &plMeterSize_.x, 0.5f, 0.0f, 1280.0f);
+	ImGui::DragFloat2("メーターサイズ", &plMeterSize_.x, 0.5f, 0.0f, 1280.0f);
+	ImGui::DragFloat2("フレームサイズ", &plFrameSize_.x, 0.5f, 0.0f, 1280.0f);
 
 	ImGui::DragFloat("縦の最大サイズ", &PLyMax_, 0.5f, 1.0f, 1280.0f);
 
