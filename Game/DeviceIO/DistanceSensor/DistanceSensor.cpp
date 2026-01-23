a4f495345f107ae2e3047efa047e83704ebfc4f7
kura495 <k024g6001@g.neec.ac.jp>
2026-01-23 15:11:17 +0900

    取ることがとりあえずできた

--- Game/DeviceIO/DistanceSensor/DistanceSensor.cpp
+++ Game/DeviceIO/DistanceSensor/DistanceSensor.cpp
@@ -9,38 +9,38 @@
 std::atomic<bool> is_running(true);
 
 void handle_signal(int signal) {
-    if (signal == SIGINT) {
-        is_running = false;
-    }
+	if (signal == SIGINT) {
+		is_running = false;
+	}
 }
 
 void DistanceSensor::Init() {
-    const auto ports = PacketIO::find_ports(PacketIO::DeviceType::PICO);
-    if (ports.empty()) {
-        std::cerr << "Port not found." << std::endl;
-        packet_io = nullptr;
-    }
+	const auto ports = PacketIO::find_ports(PacketIO::DeviceType::PICO);
+	if (ports.empty()) {
+		std::cerr << "Port not found." << std::endl;
+		packet_io = nullptr;
+	}
 
-    std::cout << "Found " << ports.size() << " ports:" << std::endl;
-    for (const auto& port : ports) {
-        std::cout << "  " << port.to_string() << std::endl;
-    }
-    
-    auto port = ports[0];
-    std::cout << "Connect to:" << std::endl << "  " << port.to_string() << std::endl;
-    packet_io = std::make_unique<PacketIO>(port);
-    if (!packet_io->open()) {
-        std::cerr << "Failed to open" << std::endl;
-        packet_io = nullptr;
-    }
+	std::cout << "Found " << ports.size() << " ports:" << std::endl;
+	for (const auto& port : ports) {
+		std::cout << "  " << port.to_string() << std::endl;
+	}
 
-    serialThread = std::thread(&DistanceSensor::Thread, this);
+	auto port = ports[0];
+	std::cout << "Connect to:" << std::endl << "  " << port.to_string() << std::endl;
+	packet_io = std::make_unique<PacketIO>(port);
+	if (!packet_io->open()) {
+		std::cerr << "Failed to open" << std::endl;
+		packet_io = nullptr;
+	}
+
+	serialThread = std::thread(&DistanceSensor::Thread, this);
 }
 
 void DistanceSensor::Update()
 {
 
-   
+
 }
 
 void DistanceSensor::Draw()
@@ -48,51 +48,116 @@ void DistanceSensor::Draw()
 	ImGui::Begin("Distance Sensor");
 	ImGui::Text(output.c_str());
 	ImGui::End();
-    CheckPosition();
+	CheckPosition();
 }
 
 void DistanceSensor::Thread() {
-    std::signal(SIGINT, handle_signal);
-    Packet packet;
-    while (is_running) {
-        while (packet_io && packet_io->read(&packet, sizeof(packet))) {
+	std::signal(SIGINT, handle_signal);
+	Packet packet;
+	while (is_running) {
+		while (packet_io && packet_io->read(&packet, sizeof(packet))) {
 			output.clear();
-            char line[120];
+			char line[120];
 
-            for (int y = 0; y < Packet::HEIGHT; y++) {
-                int offset = snprintf(line, sizeof(line), "Y=%d: ", y);
-                for (int x = 0; x < Packet::WIDTH; x++) {
-                    if (packet.status[y][x] == 5 || packet.status[y][x] == 6 ||
-                        packet.status[y][x] == 9 || packet.status[y][x] == 10) {
-                        offset += snprintf(line + offset, sizeof(line) - offset, "%4d", packet.distance_mm[y][x]);
-						//status[y].at(x) = packet.distance_mm[y][x];
-                        status[y][x] = packet.distance_mm[y][x];
-                    }
-                    else {
-                        offset += snprintf(line + offset, sizeof(line) - offset, "    ");
-                        status[y][x] = 0;
-                    }
-                    offset += snprintf(line + offset, sizeof(line) - offset, "%s", x < Packet::WIDTH - 1 ? ", " : "");
-                }
-                output += line;
-                output += "\n";
-            }
-            std::cout << output << "--------------------" << std::endl;
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    }
+			for (int y = 0; y < Packet::HEIGHT; y++) {
+				int offset = snprintf(line, sizeof(line), "Y=%d: ", y);
+				for (int x = 0; x < Packet::WIDTH; x++) {
+					if (packet.status[y][x] == 5 || packet.status[y][x] == 6 ||
+						packet.status[y][x] == 9 || packet.status[y][x] == 10) {
+						offset += snprintf(line + offset, sizeof(line) - offset, "%4d", packet.distance_mm[y][x]);
+						status[y][x] = packet.distance_mm[y][x];
+					}
+					else {
+						offset += snprintf(line + offset, sizeof(line) - offset, "    ");
+						status[y][x] = -1;
+					}
+					offset += snprintf(line + offset, sizeof(line) - offset, "%s", x < Packet::WIDTH - 1 ? ", " : "");
+				}
+				output += line;
+				output += "\n";
+			}
+			std::cout << output << "--------------------" << std::endl;
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	}
 
 }
 
 void DistanceSensor::End() {
-    is_running = false;
-    serialThread.join();
+	is_running = false;
+	serialThread.join();
 }
 
 position DistanceSensor::CheckPosition()
 {
-    ImGui::Begin("Distance Sensor2");
-    ImGui::Text("%d", status[0][0]);
-    ImGui::End();
-	return pMID;
+	//intの最大値を代入
+	// x軸はセンサーから見た位置になっている
+	//xが0~1は左を表す
+	PosData minLeft, minMid, minRight;
+	minLeft.data = std::numeric_limits<int>::max();
+	//xが2~5は真ん中を表す
+	minMid.data = std::numeric_limits<int>::max();
+	//xが6~7は右を表す
+	minRight.data = std::numeric_limits<int>::max();
+
+
+	for (int y = 0; y < HEIGHT; y++) {
+		for (int x = 0; x < WIDTH; x++) {
+			//情報が取れていない場合はスキップする
+			if (status[y][x] == -1) { 
+				continue; 
+			}
+			if (x < 1) {
+				minLeft.data += std::min<int>(minLeft.data, status[y][x]);
+				minLeft.count++;
+			}
+			else if (x < 7) {
+				minMid.data += std::min<int>(minMid.data, status[y][x]);
+				minMid.count++;
+			}
+			else if (x < 8) {
+				minRight.data += std::min<int>(minRight.data, status[y][x]);
+				minRight.count++;
+			}
+		}
+	}
+	if (minLeft.count != 0) {
+		minLeft.data /= minLeft.count;
+	}
+	if (minMid.count != 0) {
+		minMid.data /= minMid.count;
+	}
+	if (minRight.count != 0) {
+		minRight.data /= minRight.count;
+	}
+
+	// 閾値よりも全ての距離が遠い場合は何もしない
+	if (DISTANCE_THRESHOLD < minLeft.data &&
+		DISTANCE_THRESHOLD < minMid.data &&
+		DISTANCE_THRESHOLD < minRight.data
+		) {
+		return position::None;
+	}
+	ImGui::Begin("LookFromSencor");
+	ImGui::SetWindowFontScale(3.0f);
+	// 最小値が一番小さい位置を返す
+	// 左
+	if (minLeft.data <= minMid.data && minLeft.data <= minRight.data) {
+		ImGui::Text("LEFT");
+		ImGui::End();
+		return position::pLEFT;
+	}
+	// 中央
+	else if (minMid.data <= minLeft.data && minMid.data <= minRight.data) {
+		ImGui::Text("MID");
+		ImGui::End();
+		return position::pMID;
+	}
+	// 右
+	else if (minRight.data <= minLeft.data && minRight.data <= minMid.data) {
+		ImGui::Text("RIGHT");
+		ImGui::End();
+		return position::pRIGHT;
+	}
+	ImGui::End();
 }
--- Game/DeviceIO/DistanceSensor/DistanceSensor.h
+++ Game/DeviceIO/DistanceSensor/DistanceSensor.h
@@ -2,6 +2,7 @@
 #include "packet_io.h"
 #include "imgui.h"
 #include <thread>
+#include <numbers>
 
 #pragma pack(push, 1)
 struct Packet {
@@ -13,7 +14,12 @@ struct Packet {
 };
 #pragma pack(pop)
 
-enum position { pLEFT, pRIGHT, pMID };
+struct PosData{
+    uint8_t data = 0;
+    int count = 0;
+};
+
+enum position { pLEFT, pRIGHT, pMID,None };
 class DistanceSensor {
 public:
     ~DistanceSensor() {
@@ -31,9 +37,13 @@ private:
 
 
     std::unique_ptr<PacketIO>packet_io;
-
-    int16_t status[8][8];
+    static constexpr int WIDTH = 8;
+    static constexpr int HEIGHT = 8;
+    // -1はエラーを表す
+    int16_t status[HEIGHT][WIDTH];
 	std::string output;
     std::atomic<uint16_t> g_latestValue{ 0 };
     std::thread serialThread;
+
+    static constexpr int DISTANCE_THRESHOLD = 3000;
 };
