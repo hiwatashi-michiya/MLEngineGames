6adeb985ff1ee61f1055e5715bc83d2ca35c624f
hiwatashi-michiya <115054875+hiwatashi-michiya@users.noreply.github.com>
2026-02-10 01:46:31 +0900

    事前読み込みクラス不要になったので削除

--- Engines/Engine/Resource/Texture/Texture.cpp
+++ Engines/Engine/Resource/Texture/Texture.cpp
@@ -1,6 +1,5 @@
 #include "Texture.h"
 #include "TextureManager.h"
-#include "PreLoader.h"
 
 using namespace MLEngine::Resource;
 using namespace MLEngine::Core;
@@ -9,7 +8,5 @@ void Texture::Load(const std::string& filename) {
 
 	//リソースビューからインデックスに変更する
 	index_ = TextureManager::GetInstance()->Load(filename);
-
-	PreLoader::GetInstance()->AddTexture(*this);
 
 }
